222fdcb7f63c8e040c4cf47f54f39e2228ab90ac
Daniel Marino <starzonmyarmz@gmail.com>
2021-09-24 10:53:59 -0400

    Set up basic koa router

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const Koa = require('koa')
+const Router = require('@koa/router')
 const serve = require('koa-static')
 const app = new Koa()
+const router = new Router()
 app.use(serve('./static'))
+router.get('/timers', async (ctx, next) => {
+  ctx.body = 'timers'
+})
+app.use(router.routes())
+app.use(router.allowedMethods())
 app.listen(3000)
 
 console.log('http://localhost:3000')
