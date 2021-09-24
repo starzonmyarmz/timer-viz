c970ae8c6f44f1222437a1cd056eb64f23ee01a8
Daniel Marino <starzonmyarmz@gmail.com>
2021-09-24 14:34:43 -0400

    better menu

--- static/menu.js
+++ static/menu.js
@@ -1,1 +1,6 @@
 document.getElementById('menu').innerHTML = await fetch('menu.html').then((response) => { return response.text() })
+
+document.addEventListener('click', ({target}) => {
+  if (!target.closest('#menu-button')) return
+  document.getElementById('menu-options').hidden = !document.getElementById('menu-options').hidden
+})
