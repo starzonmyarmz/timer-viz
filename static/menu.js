22fae65a1bd9f4daa949164058a2b3e8a053ddcf
Daniel Marino <starzonmyarmz@gmail.com>
2021-09-27 09:59:44 -0400

    Update menu and don't require element for it

--- static/menu.js
+++ static/menu.js
@@ -1,6 +1,9 @@
-document.getElementById('menu').innerHTML = await fetch('menu.html').then((response) => { return response.text() })
+document.body.insertAdjacentHTML('beforeend', await fetch('/menu.html').then((response) => { return response.text() }))
 
 document.addEventListener('click', ({target}) => {
-  if (!target.closest('#menu-button')) return
+  if (!target.closest('.menu-button')) return
+  document.querySelectorAll('.menu-button').forEach((el) => {
+    el.hidden = !el.hidden
+  })
   document.getElementById('menu-options').hidden = !document.getElementById('menu-options').hidden
 })
